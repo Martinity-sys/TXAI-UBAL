21640b9abc676a612a2643a8d7f1387242f8e791
Jiri Derks <jiri_derks@hotmail.com>
2025-04-12 14:52:16 +0200

    readme and zo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TXAI-UBAL
 Trustworthy and Explainable AI Project - Uncertainty Based Active Learning
+
+For this project we aim to reproce <em>[The power of ensembles for active learning in image classification (Beluch et al.)](https://ieeexplore.ieee.org/document/8579074)</em>. we trained 7 different models for uncertainty based active learning. We train Monte Carlo Dropout with variation ratio and variance, Monte Carlo Dropconnect with variation ratio and variance, ensembles with variation ratio and variance, and a random acquisition model. 
--- src/calc_accuracy.py
+++ src/calc_accuracy.py
@@ -15,3 +15,40 @@ std_accuracy = np.std(final_accuracies_MCD_variation)
 print("MCD_variation accuracies:")
 print(f"Mean accuracy: {mean_accuracy}")
 print(f"Standard deviation of accuracy: {std_accuracy}")
+
+final_accuracies_ENS_varR = [88.04,
+    87.5,
+    87.88,
+    87.72,
+    87.94,
+    88.31,
+    87.78,
+    87.94,
+    88.23,
+    87.35,
+    88.08,
+    87.78,
+    87.46,
+    87.98]
+mean_accuracy = np.mean(final_accuracies_ENS_varR)
+std_accuracy = np.std(final_accuracies_ENS_varR)
+print("ENS_varR accuracies:")
+print(f"Mean accuracy: {mean_accuracy}")
+print(f"Standard deviation of accuracy: {std_accuracy}")
+
+final_accuracies_ENS_variation = [87.49,
+86.96,
+86.46,
+87.73,
+87.6,
+87.05,
+87.44,
+87.27,
+87.35,
+87.31,
+87.5]
+mean_accuracy = np.mean(final_accuracies_ENS_variation)
+std_accuracy = np.std(final_accuracies_ENS_variation)
+print("ENS_variation accuracies:")
+print(f"Mean accuracy: {mean_accuracy}")
+print(f"Standard deviation of accuracy: {std_accuracy}")
